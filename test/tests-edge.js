404a5631da52dbe0657dbfa3a99a32b17becacc2
Steven Benner <admin@stevenbenner.com>
2012-08-21 08:25:00 -0700

    Added auto-disabling button edge case.

--- test/tests-edge.js
+++ test/tests-edge.js
@@ -11,6 +11,19 @@ $(function() {
 	});
 	$('#disable-button input').powerTip({ placement: 'e' });
 
+	// Auto-disabling button
+	$('#auto-disable-button input').on('mouseenter focus', function() {
+		var button = $(this);
+		setTimeout(function() {
+			button.attr('disabled', true);
+			// enable after 2 seconds
+			setTimeout(function() {
+				button.attr('disabled', false);
+			}, 2000);
+		}, 2000);
+	});
+	$('#auto-disable-button input').powerTip({ placement: 'e' });
+
 	// Trapped mouse following tooltip
 	$('#trapped-mousefollow').powerTip({ followMouse: true });
 
