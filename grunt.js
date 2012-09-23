c33b3a85ca450eada90bb2fde55e3235530f31b2
Steven Benner <admin@stevenbenner.com>
2012-09-22 22:12:51 -0700

    Updated grunt build banner.

--- grunt.js
+++ grunt.js
@@ -7,11 +7,13 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: '<json:package.json>',
 		meta: {
-			banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-			'<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+			banner: '/*!\n' +
+			' <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+			' <%= pkg.homepage %>\n' +
+			' Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.author.url %>).\n' +
+			' Released under <%= _.pluck(pkg.licenses, "type").join(", ") %> license.\n' +
+			' <%= _.pluck(pkg.licenses, "url").join("\n") %>\n' +
+			'*/'
 		},
 		concat: {
 			dist: {
