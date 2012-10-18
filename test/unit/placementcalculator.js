2e528f907654cb25b63754ca9936274e492d489f
Steven Benner <admin@stevenbenner.com>
2012-10-17 18:39:32 -0700

    Added PlacementCalculator unit tests.

---
+++ test/unit/placementcalculator.js
@@ -0,0 +1,28 @@
+/*global
+	PlacementCalculator:true
+	CSSCoordinates:true*/
+$(function() {
+	'use strict';
+
+	module('Placement Calculator');
+
+	test('expose methods', function() {
+		var pc = new PlacementCalculator();
+		strictEqual(typeof pc.compute, 'function', 'compute method is defined');
+	});
+
+	test('return CSSCordinates object', function() {
+		var pc = new PlacementCalculator(),
+			element = $('<div>Some Content</div>'),
+			retVal = pc.compute(
+				element,
+				$.fn.powerTip.defaults.placement,
+				element.width(),
+				element.height(),
+				$.fn.powerTip.defaults.offset
+			);
+
+		ok(retVal instanceof CSSCoordinates);
+	});
+
+});
