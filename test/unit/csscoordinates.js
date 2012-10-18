3695ac3113849871b914eea85db14ea7c755780e
Steven Benner <admin@stevenbenner.com>
2012-10-17 19:13:19 -0700

    Added CSSCoordinates unit tests.

---
+++ test/unit/csscoordinates.js
@@ -0,0 +1,32 @@
+/*global
+	CSSCoordinates:true*/
+$(function() {
+	'use strict';
+
+	module('CSS Coordinates');
+
+	test('expose properties', function() {
+		var coords = new CSSCoordinates();
+		strictEqual(coords.top, 'auto', 'top property is defined');
+		strictEqual(coords.left, 'auto', 'left property is defined');
+		strictEqual(coords.right, 'auto', 'right property is defined');
+	});
+
+	test('expose methods', function() {
+		var coords = new CSSCoordinates();
+		strictEqual(typeof coords.set, 'function', 'set method is defined');
+	});
+
+	test('decimal values are rounded', function() {
+		var coords = new CSSCoordinates();
+
+		coords.set('top', 10.5);
+		coords.set('left', 10.4);
+		coords.set('right', 10.499);
+
+		strictEqual(coords.top, 11, 'top property was rounded up');
+		strictEqual(coords.left, 10, 'left property was rounded down');
+		strictEqual(coords.right, 10, 'right property was rounded down');
+	});
+
+});
