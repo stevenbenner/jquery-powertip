26283e521e6299718f0f7ff65095b4027beca9e9
Steven Benner <admin@stevenbenner.com>
2013-02-20 21:30:48 -0800

    Changed TooltipController test to use events.
    Part of issue #52.

--- test/unit/tooltipcontroller.js
+++ test/unit/tooltipcontroller.js
@@ -18,27 +18,23 @@ $(function() {
 			tc = new TooltipController($.fn.powerTip.defaults),
 			tipElem = $('#' + $.fn.powerTip.defaults.popupId);
 
+		expect(3);
+
+		element.on({
+			powerTipOpen: function() {
+				strictEqual(tipElem.css('opacity'), '1', 'tooltip is faded in');
+				tc.hideTip(element);
+			},
+			powerTipClose: function() {
+				strictEqual(tipElem.css('display'), 'none', 'display set to none');
+				start();
+			}
+		});
+
+		// sanity check - tooltip should be closed before testing
 		strictEqual(tipElem.css('display'), 'none', 'display set to none');
 
 		tc.showTip(element);
-
-		strictEqual(tipElem.css('display'), 'block', 'display set to block');
-		notStrictEqual(tipElem.css('opacity'), '1', 'tooltip is not faded in');
-
-		setTimeout(function() {
-			strictEqual(tipElem.css('opacity'), '1', 'tooltip is faded in');
-
-			tc.hideTip(element);
-
-			setTimeout(function() {
-				notStrictEqual(tipElem.css('opacity'), '0', 'tooltip is not faded out');
-			}, $.fn.powerTip.defaults.fadeOutTime - 10);
-
-			setTimeout(function() {
-				strictEqual(tipElem.css('display'), 'none', 'display set to none');
-				start();
-			}, $.fn.powerTip.defaults.fadeOutTime + 10);
-		}, $.fn.powerTip.defaults.fadeInTime + 10);
 	});
 
 });
