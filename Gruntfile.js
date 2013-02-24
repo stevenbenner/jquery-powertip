ed2b689a7f574757acc554149bd417af5f525335
Steven Benner <admin@stevenbenner.com>
2013-02-24 01:54:09 -0800

    Rewrote Grunt config file for Grunt version 0.4.
    Part of Grunt 0.4 upgrade. Issue #53.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,70 +5,100 @@
 module.exports = function(grunt) {
 	'use strict';
 
+	// configure grunt
 	grunt.initConfig({
-		pkg: '<json:package.json>',
-		meta: {
-			banner: '/*!\n' +
-			' <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-			' <%= pkg.homepage %>\n' +
-			' Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.author.url %>).\n' +
-			' Released under <%= _.pluck(pkg.licenses, "type").join(", ") %> license.\n' +
-			' <%= _.pluck(pkg.licenses, "url").join("\n") %>\n' +
-			'*/'
+		pkg: grunt.file.readJSON('package.json'),
+		buildpath: 'dist/<%= pkg.version %>',
+		files: {
+			cat: 'jquery.powertip-<%= pkg.version %>.js',
+			min: 'jquery.powertip-<%= pkg.version %>.min.js'
+		},
+		banner: [
+			'/*!',
+			' <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>',
+			' <%= pkg.homepage %>',
+			' Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.author.url %>).',
+			' Released under <%= _.pluck(pkg.licenses, "type").join(", ") %> license.',
+			' <%= _.pluck(pkg.licenses, "url").join("\\n ") %>',
+			'*/\n'
+		].join('\n'),
+		jshint: {
+			grunt: {
+				src: [ 'Gruntfile.js' ],
+				options: {
+					jshintrc: '.jshintrc'
+				}
+			},
+			tests: {
+				src: [ 'test/**/*.js' ],
+				options: {
+					jshintrc: 'test/.jshintrc'
+				}
+			},
+			dist: {
+				src: [ '<%= buildpath %>/<%= files.cat %>' ],
+				options: {
+					jshintrc: 'src/.jshintrc'
+				}
+			}
 		},
 		concat: {
 			dist: {
 				src: [
-					'<banner:meta.banner>',
-					'<file_strip_banner:src/intro.js>',
-					'<file_strip_banner:src/core.js>',
-					'<file_strip_banner:src/csscoordinates.js>',
-					'<file_strip_banner:src/displaycontroller.js>',
-					'<file_strip_banner:src/placementcalculator.js>',
-					'<file_strip_banner:src/tooltipcontroller.js>',
-					'<file_strip_banner:src/utility.js>',
-					'<file_strip_banner:src/outro.js>'
+					'src/intro.js',
+					'src/core.js',
+					'src/csscoordinates.js',
+					'src/displaycontroller.js',
+					'src/placementcalculator.js',
+					'src/tooltipcontroller.js',
+					'src/utility.js',
+					'src/outro.js'
 				],
-				dest: 'dist/jquery.powertip-<%= pkg.version %>.js'
-			}
-		},
-		min: {
-			dist: {
-				src: [
-					'<banner:meta.banner>',
-					'<config:concat.dist.dest>'
-				],
-				dest: 'dist/jquery.powertip-<%= pkg.version %>.min.js'
+				dest: '<%= buildpath %>/<%= files.cat %>',
+				options: {
+					banner: '<%= banner %>',
+					stripBanners: true
+				}
 			}
 		},
 		qunit: {
-			files: [
-				'test/index.html'
-			]
+			files: [ 'test/index.html' ]
 		},
-		lint: {
-			dist: 'dist/jquery.powertip-<%= pkg.version %>.js',
-			grunt: 'grunt.js',
-			tests: 'test/**/*.js'
+		uglify: {
+			dist: {
+				files: {
+					'<%= buildpath %>/<%= files.min %>': '<%= buildpath %>/<%= files.cat %>'
+				},
+				options: {
+					banner: '<%= banner %>'
+				}
+			}
 		},
 		watch: {
-			files: [
-				'<config:lint.grunt>',
-				'<config:lint.tests>',
-				'src/**/*.js'
-			],
-			tasks: 'lint:grunt lint:tests concat lint:dist'
-		},
-		jshint: {
-			dist: grunt.file.readJSON('src/.jshintrc'),
-			tests: grunt.file.readJSON('test/.jshintrc'),
-			grunt: grunt.file.readJSON('.jshintrc')
-		},
-		uglify: {}
+			grunt: {
+				files: [ 'Gruntfile.js' ],
+				tasks: [ 'jshint:grunt' ]
+			},
+			src: {
+				files: [ 'src/**/*.js' ],
+				tasks: [ 'concat', 'jshint:dist', 'qunit' ]
+			},
+			tests: {
+				files: [ 'test/**/*.js' ],
+				tasks: [ 'jshint:tests', 'qunit' ]
+			}
+		}
 	});
 
-	grunt.registerTask('default', 'lint:grunt lint:tests concat lint:dist qunit min');
+	// load grunt plugins
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('travis', 'lint:grunt lint:tests concat lint:dist qunit');
+	// register grunt tasks
+	grunt.registerTask('default', [ 'jshint:grunt', 'jshint:tests', 'concat', 'jshint:dist', 'qunit', 'uglify' ]);
+	grunt.registerTask('travis', [ 'jshint:grunt', 'jshint:tests', 'concat', 'jshint:dist', 'qunit' ]);
 
 };
