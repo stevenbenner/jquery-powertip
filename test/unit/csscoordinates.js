e6ed5fb0919bff11d462597ce940b13e69843f6c
Steven Benner <admin@stevenbenner.com>
2013-03-30 12:59:07 -0700

    Added bottom property to CSSCordinates() tests.
    Part of issue #31.

--- test/unit/csscoordinates.js
+++ test/unit/csscoordinates.js
@@ -8,6 +8,7 @@ $(function() {
 		strictEqual(coords.top, 'auto', 'top property is defined');
 		strictEqual(coords.left, 'auto', 'left property is defined');
 		strictEqual(coords.right, 'auto', 'right property is defined');
+		strictEqual(coords.bottom, 'auto', 'bottom property is defined');
 	});
 
 	test('expose methods', function() {
@@ -21,10 +22,12 @@ $(function() {
 		coords.set('top', 10.5);
 		coords.set('left', 10.4);
 		coords.set('right', 10.499);
+		coords.set('bottom', 10.50000000000001);
 
 		strictEqual(coords.top, 11, 'top property was rounded up');
 		strictEqual(coords.left, 10, 'left property was rounded down');
 		strictEqual(coords.right, 10, 'right property was rounded down');
+		strictEqual(coords.bottom, 11, 'bottom property was rounded up');
 	});
 
 });
