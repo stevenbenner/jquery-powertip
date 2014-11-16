9422ff6fa9f1ce6096dbe1961d80b33d850704c7
Steven Benner <admin@stevenbenner.com>
2014-11-16 14:21:38 -0800

    Added PlacementCalculator returned properties tests.

--- test/unit/placementcalculator.js
+++ test/unit/placementcalculator.js
@@ -21,4 +21,55 @@ $(function() {
 
 		ok(retVal instanceof CSSCoordinates);
 	});
+
+	test('return expected CSSCordinates properties', function() {
+		var pc = new PlacementCalculator(),
+			element = $('<div>Some Content</div>'),
+			coords;
+
+		$.each($.fn.powerTip.smartPlacementLists, function(key) {
+			coords = pc.compute(
+				element,
+				key,
+				element.width(),
+				element.height(),
+				$.fn.powerTip.defaults.offset
+			);
+
+			switch (key) {
+			case 'n':
+			case 'ne':
+			case 'nw-alt':
+				strictEqual(coords.top, 'auto', key + ': top property is set to auto');
+				strictEqual($.isNumeric(coords.left), true, key + ': left property is set to a number');
+				strictEqual(coords.right, 'auto', key + ': right property is set to auto');
+				strictEqual($.isNumeric(coords.bottom), true, key + ': bottom property is set to a number');
+				break;
+			case 'e':
+			case 's':
+			case 'se':
+			case 'sw-alt':
+				strictEqual($.isNumeric(coords.top), true, key + ': top property is set to a number');
+				strictEqual($.isNumeric(coords.left), true, key + ': left property is set to a number');
+				strictEqual(coords.right, 'auto', key + ': right property is set to auto');
+				strictEqual(coords.bottom, 'auto', key + ': bottom property is set to auto');
+				break;
+			case 'w':
+			case 'sw':
+			case 'se-alt':
+				strictEqual($.isNumeric(coords.top), true, key + ': top property is set to a number');
+				strictEqual(coords.left, 'auto', key + ': left property is set to auto');
+				strictEqual($.isNumeric(coords.right), true, key + ': right property is set to a number');
+				strictEqual(coords.bottom, 'auto', key + ': bottom property is set to auto');
+				break;
+			case 'nw':
+			case 'ne-alt':
+				strictEqual(coords.top, 'auto', key + ': top property is set to auto');
+				strictEqual(coords.left, 'auto', key + ': left property is set to auto');
+				strictEqual($.isNumeric(coords.right), true, key + ': right property is set to a number');
+				strictEqual($.isNumeric(coords.bottom), true, key + ': bottom property is set to a number');
+				break;
+			}
+		});
+	});
 });
