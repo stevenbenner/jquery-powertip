c74656e4ed6681f98a53088b6cb0f546df806a95
Steven Benner <admin@stevenbenner.com>
2016-03-27 12:07:25 -0700

    Updated QUnit references to version 1.23.0.

--- test/amd.js
+++ test/amd.js
@@ -1,7 +1,7 @@
 require.config({
 	paths: {
 		jquery: 'http://code.jquery.com/jquery-1.12.2',
-		qunit: 'http://code.jquery.com/qunit/qunit-1.22.0',
+		qunit: 'http://code.jquery.com/qunit/qunit-1.23.0',
 		'jquery.powertip': '../dist/jquery.powertip'
 	}
 });
