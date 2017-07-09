675683c1591408b37b94375ba7ad1fe0087aa4a1
Steven Benner <admin@stevenbenner.com>
2017-07-09 03:20:33 -0700

    Updated QUnit references to version 2.4.0

--- test/amd.js
+++ test/amd.js
@@ -3,7 +3,7 @@
 require.config({
 	paths: {
 		jquery: 'https://code.jquery.com/jquery-3.2.1',
-		qunit: 'https://code.jquery.com/qunit/qunit-2.3.3',
+		qunit: 'https://code.jquery.com/qunit/qunit-2.4.0',
 		'jquery.powertip': '../dist/jquery.powertip'
 	}
 });
