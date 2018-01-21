4f8977f559399a4570ab392c5a810979b2a92d33
Steven Benner <admin@stevenbenner.com>
2018-01-21 00:38:54 -0800

    Updated jQuery references to version 3.3.1

--- test/amd.js
+++ test/amd.js
@@ -2,7 +2,7 @@
 
 require.config({
 	paths: {
-		jquery: 'https://code.jquery.com/jquery-3.2.1',
+		jquery: 'https://code.jquery.com/jquery-3.3.1',
 		qunit: 'https://code.jquery.com/qunit/qunit-2.5.0',
 		'jquery.powertip': '../dist/jquery.powertip'
 	}
