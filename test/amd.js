e0465d46dda0425fea20cec0ae8980a96d841e5a
Steven Benner <admin@stevenbenner.com>
2018-11-11 20:13:52 -0800

    Update QUnit references to version 2.8.0

--- test/amd.js
+++ test/amd.js
@@ -3,7 +3,7 @@
 require.config({
 	paths: {
 		jquery: 'https://code.jquery.com/jquery-3.3.1',
-		qunit: 'https://code.jquery.com/qunit/qunit-2.7.1',
+		qunit: 'https://code.jquery.com/qunit/qunit-2.8.0',
 		'jquery.powertip': '../dist/jquery.powertip'
 	}
 });
