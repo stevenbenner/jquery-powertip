89a916d7b863c048b28bc605c519c6e6f44bc2ce
Steven Benner <admin@stevenbenner.com>
2021-03-07 06:40:07 -0800

    Update jQuery references to version 3.6.0

--- test/amd.js
+++ test/amd.js
@@ -2,7 +2,7 @@
 
 require.config({
 	paths: {
-		jquery: 'https://code.jquery.com/jquery-3.5.1',
+		jquery: 'https://code.jquery.com/jquery-3.6.0',
 		qunit: 'https://code.jquery.com/qunit/qunit-2.14.0',
 		'jquery.powertip': '../dist/jquery.powertip'
 	}
